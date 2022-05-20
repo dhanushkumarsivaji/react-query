f6ed79b7b9eda46f8513ccb0580cd1e9679de169
Dhanush S <dhas@WKMZT7418515.global.publicisgroupe.net>
2022-05-20 12:06:50 +0530

    learned about refetchOnWindowFocus & refetchOnMount

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -10,7 +10,12 @@ export const RQSuperHeroesPage = () => {
   const { isLoading, data, isError, error, isFetching } = useQuery(
     "super-heros",
     fetchSuperHeros,
-    // { cacheTime: 500000 }
+    { 
+      // cacheTime: 500000, /* By default all the results are cached to 5mins */
+      // staleTime: 30000 /*  Staletime is the time limit set of refetching. During this time the refetch will not occur. Bydefault it is set to zero.*/
+      // refetchOnMount: true,
+      // refetchOnWindowFocus: true,
+    }
   );
 
   console.log({ isLoading, isFetching });
@@ -27,3 +32,20 @@ export const RQSuperHeroesPage = () => {
     </Fragment>
   );
 };
+
+// refetchOnMount: boolean | "always" | ((query: Query) => boolean | "always")
+// Optional
+// Defaults to true
+// If set to true, the query will refetch on mount if the data is stale.
+// If set to false, the query will not refetch on mount.
+// If set to "always", the query will always refetch on mount.
+// If set to a function, the function will be executed with the query to compute the value
+
+
+// refetchOnWindowFocus: boolean | "always" | ((query: Query) => boolean | "always")
+// Optional
+// Defaults to true
+// If set to true, the query will refetch on window focus if the data is stale.
+// If set to false, the query will not refetch on window focus.
+// If set to "always", the query will always refetch on window focus.
+// If set to a function, the function will be executed with the query to compute the value
