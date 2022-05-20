2e09e4eaaf7e82fe5fcb7fd06e9daf240ab940ff
Dhanush S <dhas@WKMZT7418515.global.publicisgroupe.net>
2022-05-20 11:24:58 +0530

    incorporated react-query devtools

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -11,6 +12,7 @@ ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right"/>
     </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
