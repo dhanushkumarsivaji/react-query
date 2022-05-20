f95b677d8f0a51a78d4588c12dd41a3e4ecfec0a
Dhanush S <dhas@WKMZT7418515.global.publicisgroupe.net>
2022-05-20 11:31:56 +0530

    learned how query cache works

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,23 +1,29 @@
-import { useQuery } from 'react-query';
-import axios from 'axios';
-import { Fragment } from 'react/cjs/react.production.min';
+import { useQuery } from "react-query";
+import axios from "axios";
+import { Fragment } from "react/cjs/react.production.min";
 
-const fetchSuperHeros = ()  => {
-  return axios.get('http://localhost:4000/superheroes')
-}
+const fetchSuperHeros = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data, isError, error } = useQuery('super-heros',fetchSuperHeros)
+  const { isLoading, data, isError, error, isFetching } = useQuery(
+    "super-heros",
+    fetchSuperHeros,
+    // { cacheTime: 500000 }
+  );
 
-  if(isLoading) return <h2>Loading....</h2>
+  console.log({ isLoading, isFetching });
 
-  if(isError) return <h2>{error.message}</h2>
+  if (isLoading) return <h2>Loading....</h2>;
+
+  if (isError) return <h2>{error.message}</h2>;
   return (
     <Fragment>
       <h2>React Query Super Heroes Page</h2>
       {data.data.map((hero) => {
-        return <div key={hero.name}>{hero.name}</div>
+        return <div key={hero.name}>{hero.name}</div>;
       })}
     </Fragment>
-    )
-}
+  );
+};
